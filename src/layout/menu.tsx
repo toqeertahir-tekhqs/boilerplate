aa2e3bd16cac6f148407e124129ceafdcce95d7a
toqeertahir-tekhqs <toqeer.tahir@tekhqs.com>
2025-02-04 16:10:48 +0500

    menu settin incomplte

--- src/layout/menu.tsx
+++ src/layout/menu.tsx
@@ -1,21 +1,41 @@
+import { Menu, MenuProps } from 'antd';
 import { brytAssets } from "assets";
+import { useState } from 'react';
 import { FiBell } from "react-icons/fi";
 import { LuSettings } from "react-icons/lu";
 
 const MenuAntD = () => {
+    type MenuItem = Required<MenuProps>['items'][number];
+    const [current, setCurrent] = useState('mail');
 
+    const onClick: MenuProps['onClick'] = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+    const items: MenuItem[] = [
+        {
+            label: 'Navigation One',
+            key: 'mail',
+        },
+        {
+            label: 'Navigation Two',
+            key: 'mail1',
+        },
+        {
+            label: 'Navigation Three',
+            key: 'mail2',
+        },
+        {
+            label: 'Navigation fouth',
+            key: 'mail3',
+        },
+    ]
     return (
         <>
             <div>
                 <img src={brytAssets.BrytLogo} alt="Bryt Logo" />
             </div>
-            <div className="text-[#000000] flex gap-5">
-                <div className="menu-selected px-3 py-3 ">Dashboard</div>
-                <div className="menu-selected px-3 py-3 ">Chit Fund Group</div>
-                <div className="menu-selected px-3 py-3 ">Transactions</div>
-                <div className="menu-selected px-3 py-3 ">Smart Contracts</div>
-                <div className="menu-selected px-3 py-3 ">KYC/AML Compliance</div>
-            </div>
+            <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
             <div className="flex gap-5 items-center">
                 <div>
                     <LuSettings size={20} />
