a41bef1cd38b3f071bb1fcf34efae9ef942b0036
toqeertahir-tekhqs <toqeer.tahir@tekhqs.com>
2025-02-05 08:30:37 +0500

    menu UI

--- src/layout/menu.tsx
+++ src/layout/menu.tsx
@@ -1,12 +1,15 @@
+import { MoreOutlined } from '@ant-design/icons';
 import { Menu, MenuProps } from 'antd';
 import { brytAssets } from "assets";
 import { useState } from 'react';
 import { FiBell } from "react-icons/fi";
 import { LuSettings } from "react-icons/lu";
+import { useNavigate } from 'react-router-dom';
 
 const MenuAntD = () => {
     type MenuItem = Required<MenuProps>['items'][number];
-    const [current, setCurrent] = useState('mail');
+    const [current, setCurrent] = useState('/');
+    const navigate = useNavigate()
 
     const onClick: MenuProps['onClick'] = (e) => {
         console.log('click ', e);
@@ -14,28 +17,63 @@ const MenuAntD = () => {
     };
     const items: MenuItem[] = [
         {
-            label: 'Navigation One',
-            key: 'mail',
+            label: 'Dashboard',
+            key: '/table-test',
+            onClick: () => {
+                navigate('/table-test')
+            }
         },
         {
-            label: 'Navigation Two',
-            key: 'mail1',
+            label: 'Chit Fund Group',
+            key: '/chit-fund-group',
+            onClick: () => {
+                navigate('/chit-fund-group')
+            }
         },
         {
-            label: 'Navigation Three',
-            key: 'mail2',
+            label: 'Transactions',
+            key: '/transactions',
+            onClick: () => {
+                navigate('/transactions')
+            }
         },
         {
-            label: 'Navigation fouth',
-            key: 'mail3',
+            // label: 'Navigation Three - Submenu',
+            key: 'SubMenu',
+            icon: <MoreOutlined style={{ transform: 'rotate(90deg)', marginTop: '12px' }} />,
+            children: [
+                {
+                    label: 'Smart Contracts',
+                    key: '/smart-contracts',
+                    onClick: () => {
+                        navigate('/smart-contracts')
+                    }
+                },
+                {
+                    label: 'KYC/AML Compliance',
+                    key: '/kyc-compliance',
+                    onClick: () => {
+                        navigate('/kyc-compliance')
+                    }
+                },
+                {
+                    label: 'Dispute',
+                    key: '/dispute',
+                    onClick: () => {
+                        navigate('/dispute')
+                    }
+                },
+            ],
         },
-    ]
+    ];
     return (
         <>
             <div>
                 <img src={brytAssets.BrytLogo} alt="Bryt Logo" />
             </div>
-            <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+            <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items}
+                theme="light"
+            />
             <div className="flex gap-5 items-center">
                 <div>
                     <LuSettings size={20} />
